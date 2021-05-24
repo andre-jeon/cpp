1a8a24faee9b2642042a9d21d5327ad883f617c2
Myeongwon André Jeon <maj@Myeongwons-MBP.fios-router.home>
2021-05-24 15:47:21 -0400

    double done

--- Lesson2/radius.cpp
+++ Lesson2/radius.cpp
@@ -1,10 +1,13 @@
 /*Let C be a circle with a radius of length r
 We have: (Area of C) = PI * r^2*/
 
-#include <iostream>
+#include <iostream> // extension that allows us to use cin and cout
+#include <cmath> // extension that allows us to use built in Pi value 
 using namespace std;
 
-const double PI = 3.14;
+
+// Give it a constant value for PI
+// const double PI = 3.14; M_PI is much more accurate representation of Pi
 
 int main() {
     double radius, area;
@@ -12,9 +15,9 @@ int main() {
     cout << "Please enter the radius:" << endl;
     cin >> radius;
 
-    area = PI * (radius * radius);
+    area = M_PI * (radius * radius);
 
-    cout << "The area of a circle with radius of " << radius << " is " << area << "cm." << endl;
+    cout << "The area of a circle with radius of " << radius << " is " << area << endl;
 
     return 0;
 }
