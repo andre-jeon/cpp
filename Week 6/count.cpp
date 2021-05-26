764699abc0147a1f54fd5817bd3ce6705db91774
Myeongwon André Jeon <maj@Myeongwons-MBP.fios-router.home>
2021-05-26 13:21:34 -0400

    while loop approach

--- Week 6/count.cpp
+++ Week 6/count.cpp
@@ -16,6 +16,28 @@ Please enter a positive integer: 4
 using namespace std;
 
 int main() {
+    int n;
+    int counter;
+
+    // when the program runs, this message shows up
+    cout << "Please enter a positive integer:";
+
+    // user input n
+    cin >> n;
+
+    // initial counter value
+    counter = 1;
+
+    // while counter is less than or equal to n
+    // the while loop is executed as long as the value of counter is n or less
+    while(counter <= n) {
+
+        // printing the value of counter
+        cout << counter << endl;
+
+        // then incrementing counter by 1
+        counter++;
+    }
 
     return 0;
 }
