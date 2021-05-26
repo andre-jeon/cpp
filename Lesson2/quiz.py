7e254bfcfc61c6c3e9a673314584261d7b5b6b75
Myeongwon André Jeon <maj@Myeongwons-MBP.fios-router.home>
2021-05-26 13:11:01 -0400

    week 6 start

--- Lesson2/quiz.py
+++
@@ -1,7 +0,0 @@
-class Solutions:
-    def quiz():
-        int_result = 20 + 6 * 2
-
-        return int_result
-    
-    print(quiz())
---
+++ Week 6/count.cpp
@@ -0,0 +1,21 @@
+/* 
+
+Write a program that reads a positive integer n, and prints the numbers from 1 up to n 
+
+Example:
+Please enter a positive integer: 4
+
+1
+2
+3
+4
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
